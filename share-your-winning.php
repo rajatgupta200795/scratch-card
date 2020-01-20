134c84e9d608b3c638f6a5a05b5d2aa6e6d2a87b
rajat gupta <rajatgupta200795@gmail.com>
2020-01-20 06:04:28 +0000

    second commit for prod

---
+++ share-your-winning.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+<title>Earn Paytm Cash - devopsrider.com</title>
+
+    <meta name="viewport" content="width=400">
+
+
+
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'  type='text/css'>
+
+
+</head>
+<body>
+
+<?php
+
+$nameValue = @$_POST['nameValue'];
+$numberValue = @$_POST['numberValue'];
+$priceValue = @$_POST['priceValue'];
+
+?>
+
+<center>
+
+<img src="https://img.icons8.com/color/144/000000/paytm.png" style="width:100px;">
+
+<div id="showShareMssg" class="hidde">
+        <div style="padding:10px; color: green; font-size:20px;">Thanks <?php echo $nameValue; ?> ! we have got your Paytm number <?php echo $numberValue; ?>. Please share your winning with 5 friends to get Rs.<?php echo $priceValue; ?>/-</div>
+        <br>
+        <span style="color: red; font-size:17px;"><b>Note: </b></span>If you do not share with friends, you will not get winning amount.
+        <br><br><br>
+        <a class="btn btn-lg btn-success" href="<?php echo "whatsapp://send?text=Are mere ko scratch card se Rs. ".$priceValue." paytm cashback mila hai. Mane aapko bhi ek scratch card bheja hai. click https://bit.ly/3a6NWX6"; ?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp" aria-hidden="true"></i>
+Share with 5 Friends</a>
+        <br><br>
+Once your friends open their scratch card, you will get paytm cashback.
+        <div style="color: ; font-size:17px;">Please refresh and wait for 2 minutes. If you do not get cashback within 2 hours then visit <a href="http://www.devopsrider.com">here</a> to get solution.</div>
+<div>Scroll for more details. <a>terms and conditions apply.</a></div>
+<br>
+</div>
+
+
+
+</center>
+
+<br><br>
+<hr>
+
+<div style="font-size:10px; text-align:center;">To earn paytm cash scratch the card and win amount upto 500/- Share your winning story with your friends and get transferred paytm amount direct to your paytm wallet. You can also earn cashback with Google pay. Pay bills and do recharge to get Google pay scratch card and win money upto Rs. 1000/- <br>
+Google pay is the India's number one online payment platform. You can transfer money from acount to acount through Google pay UPI.<br>
+Today Amazon pay is also emerging as an online transaction platform in India. They are also providing multiple scheme to earn Amazon pay cashback. Apart from this Flipkart has also started supercoin offer to attract customers.
+</div>
+
+<hr>
+
+</body>
+</html>
